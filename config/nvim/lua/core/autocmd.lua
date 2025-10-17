9cc31825aca6cb75284ab5a5d28f830342682460
Daya <141539624+Daya1222@users.noreply.github.com>
2025-10-17 22:12:10 +0545

    Update autocmd.lua

--- config/nvim/lua/core/autocmd.lua
+++ config/nvim/lua/core/autocmd.lua
@@ -9,3 +9,11 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.smartindent = true -- optional
 	end,
 })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
